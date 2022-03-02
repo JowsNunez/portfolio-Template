809bc477cb6cd5acd2db0a7ee2eed935e9b786d5
Jose N <jose.nunez181930@potros.itson.edu.mx>
2022-03-02 14:07:28 -0700

    Fixed changes themes

--- src/js/letters.js
+++ src/js/letters.js
@@ -4,10 +4,28 @@ document.documentElement.className =localStorage.getItem('theme')
 
 
 const setTheme = (theme) => {
-    document.documentElement.className = theme
+    document.documentElement.toggleAttribute ( theme)
+    document.documentElement.className=theme
     localStorage.setItem('theme', theme);
 };
 
-document.getElementById('theme-select').addEventListener('change', function () {
-    setTheme(this.value);
-});
+// document.getElementById('theme-select').addEventListener('change', function () {
+//     setTheme( this.value);
+   
+// });
+
+
+let query = document.querySelectorAll('.change');
+
+
+
+query.forEach(item =>{
+   item.addEventListener('click', e=>{
+    setTheme( e.target.value);
+   })
+})
+
+window.addEventListener('change',()=>{
+
+
+})
